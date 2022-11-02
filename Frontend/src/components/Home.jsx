5138efef02764764d77eb44184f72bea4cf52d76
kartikmishraa <kartikm2219@gmail.com>
2022-11-03 02:18:22 +0530

    temp

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -4,25 +4,25 @@ import sellerLogo from "./Assets/seller.png";
 import customerLogo from "./Assets/customer.png";
 
 const Home = () => {
-  const sellerWeb = () =>{
-    window.location.pathname="/seller/login"
-  }
-  const customerWeb = () =>{
-    window.location.pathname="/customer/login"
-  }
-  
+  const sellerWeb = () => {
+    window.location.pathname = "/seller/login";
+  };
+  const customerWeb = () => {
+    window.location.pathname = "/customer/login";
+  };
+
   return (
     <>
       <div className="main-container">
         <div className="wrapper">
-            <div className="seller" onClick={sellerWeb}>
-              <img src={sellerLogo} alt="Seller" />
-              <b className="margin-top-home">Seller</b>
-            </div>
-            <div className="customer" onClick={customerWeb}>
-              <img src={customerLogo} alt="" />
-              <b className="margin-top-home">Customer</b>
-            </div>
+          <div className="seller" onClick={sellerWeb}>
+            <img src={sellerLogo} alt="Seller" />
+            <b className="margin-top-home">Seller</b>
+          </div>
+          <div className="customer" onClick={customerWeb}>
+            <img src={customerLogo} alt="" />
+            <b className="margin-top-home">Customer</b>
+          </div>
         </div>
       </div>
     </>
